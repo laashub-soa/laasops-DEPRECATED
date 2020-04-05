fbc63b5548e35a8b401994aa1a4a0a0ad5025c12
tristan-tsl <tanshilinmail@gmail.com>
2020-04-05 11:51:09 +0800

    init the DesignerDataDirectory

--- workstation/src/component/designer/designer_data/designer_data_directory/DesignerDataDirectory.vue
+++ workstation/src/component/designer/designer_data/designer_data_directory/DesignerDataDirectory.vue
@@ -1,18 +1,57 @@
 <template>
-  <div id="DesignerDataDirectory">
-
+  <div>
+    <button @click="addNode">Add Node</button>
+    <vue-tree-list
+      @click="onClick"
+      @change-name="onChangeName"
+      @delete-node="onDel"
+      @add-node="onAddNode"
+      :model="data"
+      default-tree-node-name="new node"
+      default-leaf-node-name="new leaf"
+      v-bind:default-expanded="true">
+    </vue-tree-list>
+    <button @click="getNewTree">Get new tree</button>
+    <pre>
+      {{newTree}}
+    </pre>
   </div>
 </template>
 
 <script>
-    // import layui from 'layui-src/dist/layui'
+    import {Tree, TreeNode, VueTreeList} from 'vue-tree-list'
     import request from "../../../../request.js";
 
     export default {
         name: "DesignerDataDirectory",
+        components: {
+            VueTreeList
+        },
         data() {
             return {
                 tree: [],
+                newTree: {},
+                data: new Tree([
+                    {
+                        name: 'Node 1',
+                        id: 1,
+                        pid: 0,
+                        // dragDisabled: true,
+                        children: [
+                            {
+                                name: 'Node 1-2',
+                                id: 2,
+                                isLeaf: true,
+                                pid: 1
+                            }
+                        ]
+                    },
+                    {
+                        name: 'Node 3',
+                        id: 4,
+                        pid: 0
+                    }
+                ])
             }
         },
         methods: {
@@ -76,12 +115,80 @@
         },
         async created() {
             await this.init_designer_data_directory();
-        }
+        },
+        onDel(node) {
+            console.log(node)
+            node.remove()
+        },
+
+        onChangeName(params) {
+            console.log(params)
+        },
+
+        onAddNode(params) {
+            console.log(params)
+        },
+
+        onClick(params) {
+            console.log(params)
+        },
+
+        addNode() {
+            var node = new TreeNode({name: 'new node', isLeaf: false})
+            if (!this.data.children) this.data.children = []
+            this.data.addChildren(node)
+        },
+
+        getNewTree() {
+            var vm = this
+
+            function _dfs(oldNode) {
+                var newNode = {}
+
+                for (var k in oldNode) {
+                    if (k !== 'children' && k !== 'parent') {
+                        newNode[k] = oldNode[k]
+                    }
+                }
+
+                if (oldNode.children && oldNode.children.length > 0) {
+                    newNode.children = []
+                    for (var i = 0, len = oldNode.children.length; i < len; i++) {
+                        newNode.children.push(_dfs(oldNode.children[i]))
+                    }
+                }
+                return newNode
+            }
+
+            vm.newTree = _dfs(vm.data)
+        },
     }
 
 </script>
 
 <style scoped>
-  @import "~layui-src/dist/css/layui.css";
   @import '~view-design/dist/styles/iview.css';
+</style>
+<style lang="less" rel="stylesheet/less">
+  .vtl {
+    .vtl-drag-disabled {
+      background-color: #d0cfcf;
+
+      &:hover {
+        background-color: #d0cfcf;
+      }
+    }
+
+    .vtl-disabled {
+      background-color: #d0cfcf;
+    }
+  }
+</style>
+
+<style lang="less" rel="stylesheet/less" scoped>
+  .icon {
+    &:hover {
+      cursor: pointer;
+    }
+  }
 </style>
