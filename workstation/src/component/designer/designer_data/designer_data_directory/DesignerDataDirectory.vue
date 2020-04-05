2c30e0cf913ef9046b66b350366ce6683a6834da
tristan-tsl <tanshilinmail@gmail.com>
2020-04-05 11:56:52 +0800

    init the DesignerDataDirectory

--- workstation/src/component/designer/designer_data/designer_data_directory/DesignerDataDirectory.vue
+++ workstation/src/component/designer/designer_data/designer_data_directory/DesignerDataDirectory.vue
@@ -112,56 +112,57 @@
             async init_tree_view() {
 
             },
+            onDel(node) {
+                console.log(node)
+                node.remove()
+            },
+
+            onChangeName(params) {
+                console.log(params)
+            },
+
+            onAddNode(params) {
+                console.log(params)
+            },
+
+            onClick(params) {
+                console.log(params)
+            },
+
+            addNode() {
+                var node = new TreeNode({name: 'new node', isLeaf: false})
+                if (!this.data.children) this.data.children = []
+                this.data.addChildren(node)
+            },
+
+            getNewTree() {
+                var vm = this
+
+                function _dfs(oldNode) {
+                    var newNode = {}
+
+                    for (var k in oldNode) {
+                        if (k !== 'children' && k !== 'parent') {
+                            newNode[k] = oldNode[k]
+                        }
+                    }
+
+                    if (oldNode.children && oldNode.children.length > 0) {
+                        newNode.children = []
+                        for (var i = 0, len = oldNode.children.length; i < len; i++) {
+                            newNode.children.push(_dfs(oldNode.children[i]))
+                        }
+                    }
+                    return newNode
+                }
+
+                vm.newTree = _dfs(vm.data)
+            },
         },
         async created() {
             await this.init_designer_data_directory();
         },
-        onDel(node) {
-            console.log(node)
-            node.remove()
-        },
 
-        onChangeName(params) {
-            console.log(params)
-        },
-
-        onAddNode(params) {
-            console.log(params)
-        },
-
-        onClick(params) {
-            console.log(params)
-        },
-
-        addNode() {
-            var node = new TreeNode({name: 'new node', isLeaf: false})
-            if (!this.data.children) this.data.children = []
-            this.data.addChildren(node)
-        },
-
-        getNewTree() {
-            var vm = this
-
-            function _dfs(oldNode) {
-                var newNode = {}
-
-                for (var k in oldNode) {
-                    if (k !== 'children' && k !== 'parent') {
-                        newNode[k] = oldNode[k]
-                    }
-                }
-
-                if (oldNode.children && oldNode.children.length > 0) {
-                    newNode.children = []
-                    for (var i = 0, len = oldNode.children.length; i < len; i++) {
-                        newNode.children.push(_dfs(oldNode.children[i]))
-                    }
-                }
-                return newNode
-            }
-
-            vm.newTree = _dfs(vm.data)
-        },
     }
 
 </script>
