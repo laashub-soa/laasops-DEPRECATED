284d76ab52a64dee0d4cc7477014b14edb734337
tristan-tsl <tanshilinmail@gmail.com>
2020-04-07 18:14:10 +0800

    finish the backend: data struct cud

--- distribution/service/data/struct.py
+++ distribution/service/data/struct.py
@@ -18,3 +18,30 @@ def select():
                 where did = %(did)s
     """, request_data))
 
+
+@app.route('/insert', methods=['POST'])
+def insert():
+    request_data = form.check(["did", "column", "comment", "reference_type"])
+    return json.dumps(mymysql.execute("""
+                insert into designer_data_struct(did, code, meaning, reference_type ) 
+                values (%(did)s, %(code)s, %(meaning)s, %(reference_type)s)
+    """, request_data))
+
+
+@app.route('/update', methods=['POST'])
+def update():
+    request_data = form.check(["id", "column", "comment", "reference_type"])
+    return json.dumps(mymysql.execute("""
+                update designer_data_struct 
+                set code = %(code)s ,meaning = %(meaning)s
+                where id = %(id)s
+    """, request_data))
+
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    request_data = form.check(["id"])
+    return json.dumps(mymysql.execute("""
+                delete from designer_data_struct
+                where id = %(id)s
+    """, request_data))
