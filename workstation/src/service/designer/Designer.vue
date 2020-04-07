53dcfefe0529c1d3089437c47cc1ca280b61a46e
tristan-tsl <tanshilinmail@gmail.com>
2020-04-07 10:38:57 +0800

    if the already have the same name tab, need to update every tab' name

--- workstation/src/service/designer/Designer.vue
+++ workstation/src/service/designer/Designer.vue
@@ -85,27 +85,35 @@
                     if (parent["parent"]) parent = parent["parent"];
                     else parent = null;
                 }
+                breadcrumb_list.pop();
                 this._data.breadcrumb.list = breadcrumb_list.reverse();
 
                 // update the right panel
-                // TODO if the already have the same name tab, need to update every tab' name
-                const tab_panel_id = _id;
-                let exist_panel_id = null;
+                const cur_service_type = this._data.menu_active_name;
+                let label = name;
+                let is_not_exist = true;
+                const tab_panel_id = cur_service_type + _id;
                 for (const index in this._data.tab_pane) {
                     const item = this._data.tab_pane[index];
                     if (item['name'] == tab_panel_id) {
+                        is_not_exist = false;
                         this._data.tab_pane_cur = parseInt(index);
-                        return;
+                        break;
+                    }
+                    if (label == item['label']) {
+                        label = breadcrumb_list.join(" / ");
                     }
                 }
-                this._data.tab_pane.push({
-                    'type': 'data',
-                    'name': tab_panel_id,
-                    'label': name,
-                    'visible': true,
-                    'icon': 'md-albums',
-                });
-                this._data.tab_pane_cur = this._data.tab_pane.length - 1;
+                if (is_not_exist) {
+                    this._data.tab_pane.push({
+                        'type': cur_service_type,
+                        'name': tab_panel_id,
+                        'label': label,
+                        'visible': true,
+                        'icon': 'md-albums',
+                    });
+                    this._data.tab_pane_cur = this._data.tab_pane.length - 1;
+                }
             },
         },
     }
