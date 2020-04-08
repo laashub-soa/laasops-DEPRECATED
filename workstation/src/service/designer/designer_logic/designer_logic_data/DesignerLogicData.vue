4d6788e02fb4556f1fb43ff2e0907ea01f489b38
tristan-tsl <tanshilinmail@gmail.com>
2020-04-08 11:37:17 +0800

    temp

---
+++ workstation/src/service/designer/designer_logic/designer_logic_data/DesignerLogicData.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    language: Python; Version: 3+;
+    <i-button type="primary" @click="standard.display = true">Show Standard</i-button>
+    <modal v-model="standard.display" title="Logic Coding Standard">
+      <pre v-html='standard.content'></pre>
+    </modal>
+    <textarea id="code" name="code"></textarea>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "DesignerLogicData",
+        props: {
+            directory_id: {
+                default: 60,
+                type: Number,
+            },
+            directory_name: {
+                default: 'test',
+                type: String,
+            },
+            split_value: {
+                default: 0.2,
+            },
+        },
+        data() {
+            return {
+                editor: null,
+                cur_writing: {
+                    id: "",
+                },
+                standard: {
+                    display: false,
+                    content: '',
+                },
+            }
+        },
+        methods: {
+            async init_editor(){
+
+            },
+        },
+        created() {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ workstation/src/service/designer/designer_logic/designer_logic_data/designer_logic_data.js
@@ -0,0 +1,50 @@
+function get_standard_content() {
+  return `from engine import Data
+from engine import Runtime
+
+Runtime.define_dependencies(["dingtalkchatbot"])
+
+
+def test(engine_data):
+    data_id = engine_data["data_id"]
+    data_data_id = engine_data["data_data_id"]
+    data_event_type = engine_data["data_event_type"]
+    logic_id = engine_data["logic_id"]
+    func_name = engine_data["func_name"]
+    print("test")
+    json = Runtime.require('json')
+    print("json: ", json)
+    print("data_id: ", data_id, "data_data_id: ", data_data_id, "data_event_type: ", data_event_type, "logic_id: ",
+          logic_id, "func_name: ", func_name)
+    sql_result = Data.get(' select * from designer_data_data_%(data_id)s where id=%(data_data_id)s ',
+                          {'data_id': data_id, 'data_data_id': data_data_id})
+    print(str(sql_result))
+    # Data.get(' select username, password from aliyun ')
+    # Data.set(' insert into aliyun(username, password) values(%(username)s, %(password)s) ',
+    #          {'username': 'xxx', 'password': 'xxx'})
+    # Data.set(' update host set password where id = %(id)s ', {'id': '1'})
+    # Data.set(' delete from aliyun where username = %(username)s ', {'username': 'xxx'})
+
+
+# start data_define
+data_define = {
+    'get': [
+        '21',
+    ],
+    'set': [
+        '21',
+    ],
+    'trigger': {
+        '21': {
+            'insert': 'test'
+        },
+    }
+}
+#  end data_define
+
+`;
+}
+
+export default {
+  get_standard_content
+}
