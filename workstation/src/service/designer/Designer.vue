8f9e2eeab5abe64d1825eb48b38f3482c0eb4636
tristan-tsl <tanshilinmail@gmail.com>
2020-04-08 10:34:02 +0800

    more perfect

--- workstation/src/service/designer/Designer.vue
+++ workstation/src/service/designer/Designer.vue
@@ -91,7 +91,6 @@
         },
         methods: {
             beforeRemove(index) {
-                console.log("remove: " + index);
                 const component = this;
                 return new Promise(function (resolve, reject) {
                     let real_index = 0;
@@ -115,7 +114,6 @@
                 })
             },
             onClickTab(index) {
-                console.log("click: " + index);
                 const tab_data = this._data.tab_pane[index];
                 // breadcrumb
                 this._data.breadcrumb.list = tab_data['breadcrumb_list'];
@@ -142,40 +140,36 @@
                 breadcrumb_list.pop();
                 this._data.breadcrumb.list = breadcrumb_list.reverse();
 
-                // update the right panel
                 const cur_service_type = this._data.menu_active_name;
                 let label = name;
-                let is_not_exist = true;
                 const tab_panel_id = cur_service_type + _id;
                 this._data.tab_panel_cur_id = tab_panel_id;
                 for (const index in this._data.tab_pane) {
                     const item = this._data.tab_pane[index];
                     if (item['name'] == tab_panel_id) {
-                        is_not_exist = false;
+                        // is_not_exist = false;
                         this._data.tab_pane_cur = parseInt(index);
                         this._data.tab_pane[index].visible = true;
-                        break;
+                        return;
                     }
                     if (label == item['label']) {
                         label = breadcrumb_list.join(" / ");
                     }
                 }
-                if (is_not_exist) {
-                    let icon = 'md-albums';
-                    if (cur_service_type == 'logic') {
-                        icon = 'md-cog';
-                    }
-                    this._data.tab_pane.push({
-                        'type': cur_service_type,
-                        'name': tab_panel_id,
-                        'directory_id': _id,
-                        'breadcrumb_list': breadcrumb_list,
-                        'label': label,
-                        'visible': true,
-                        'icon': icon,
-                    });
-                    this._data.tab_pane_cur = this._data.tab_pane.length - 1;
-                }
+                  let icon = 'md-albums';
+                  if (cur_service_type == 'logic') {
+                      icon = 'md-cog';
+                  }
+                  this._data.tab_pane.push({
+                      'type': cur_service_type,
+                      'name': tab_panel_id,
+                      'directory_id': _id,
+                      'breadcrumb_list': breadcrumb_list,
+                      'label': label,
+                      'visible': true,
+                      'icon': icon,
+                  });
+                  this._data.tab_pane_cur = this._data.tab_pane.length - 1;
             },
         },
     }
