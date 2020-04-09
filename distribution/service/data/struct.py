c516decbe8d39414d580605469357bada1e50966
tristan-tsl <tanshilinmail@gmail.com>
2020-04-09 10:49:29 +0800

    finish the data data column associate operation with data struct

--- distribution/service/data/struct.py
+++ distribution/service/data/struct.py
@@ -22,6 +22,12 @@ def select():
 @app.route('/insert', methods=['POST'])
 def insert():
     request_data = form.check(["did", "code", "meaning", "reference_type"])
+    code = request_data['code']
+    # insert column to data data
+    mymysql.execute(
+        'ALTER TABLE designer_data_data_%(did)s ADD COLUMN ' + code + ' VARCHAR(255) DEFAULT NULL COMMENT %(meaning)s;',
+        request_data)
+
     return json.dumps(mymysql.execute("""
                 insert into designer_data_struct(did, code, meaning, reference_type ) 
                 values (%(did)s, %(code)s, %(meaning)s, %(reference_type)s)
@@ -31,6 +37,12 @@ def insert():
 @app.route('/update', methods=['POST'])
 def update():
     request_data = form.check(["did", "code", "meaning", "reference_type"])
+    code = request_data['code']
+    # update column to data data
+    mymysql.execute(
+        'ALTER TABLE designer_data_data_%(did)s change ' + code + ' ' + code
+        + ' VARCHAR(255) DEFAULT NULL COMMENT %(meaning)s;',
+        request_data)
     return json.dumps(mymysql.execute("""
                 update designer_data_struct 
                 set code = %(code)s ,meaning = %(meaning)s
@@ -41,6 +53,12 @@ def update():
 @app.route('/delete', methods=['POST'])
 def delete():
     request_data = form.check(["id"])
+    code = request_data['code']
+    # delete column to data data
+    mymysql.execute(
+        'ALTER TABLE designer_data_data_%(did)s drop ' + code,
+        request_data)
+
     return json.dumps(mymysql.execute("""
                 delete from designer_data_struct
                 where id = %(id)s
