cbc21264a75c2a2370ecf3f0289ee8fb041ddc9a
tristan-tsl <tanshilinmail@gmail.com>
2020-04-10 01:57:53 +0800

    temp

--- workstation/src/main.js
+++ workstation/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
-import App from './app.vue';
+import App from './App.vue';
 import routers from './router'
 import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
